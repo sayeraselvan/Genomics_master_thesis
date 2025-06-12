2624074524347d995ab8502e83f1acf20e0cec3e
Siva Subramanian Ayeraselvan <112092883+sayeraselvan@users.noreply.github.com>
2025-06-13 03:58:32 +0530

    Update 1_signor_code_block_folders.sh

--- NGS_genomics_analysis/1_signor_code_block_folders.sh
+++ NGS_genomics_analysis/1_signor_code_block_folders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the directory where your files are located, ### scripy to make the folders with the names:
-input_dir="/home/vetlinux05/Siva/raw_data/test/signor/cut"  # Change this to your actual folder path
+input_dir="/home/Siva/raw_data/test/signor/cut"  # Change this to your actual folder path
 
 # Loop through each fastq.gz file in the directory
 for file in "$input_dir"/*_pass_trimmed.fastq.gz; do
