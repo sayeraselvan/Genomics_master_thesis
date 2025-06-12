87e7dcad211acf14ef6f7b685c34f66c5959dd73
Siva Subramanian Ayeraselvan <112092883+sayeraselvan@users.noreply.github.com>
2025-06-13 03:58:08 +0530

    Update 10_184_samples_genotypevcf.sh

--- NGS_genomics_analysis/10_184_samples_genotypevcf.sh
+++ NGS_genomics_analysis/10_184_samples_genotypevcf.sh
@@ -1,54 +1,54 @@
 # For chromosome X
 gatk --java-options "-Xmx32g -Xms32g" \
     GenotypeGVCFs \
-    -R /home/vetlinux05/Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
+    -R /home//Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
     --variant "gendb://my_database_chrX" \
     -O X_genotyped.vcf.gz \
-    --tmp-dir /home/vetlinux05/Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
+    --tmp-dir /home//Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
     >> X_genotype.log 2>&1
 
 # For chromosome 2L
 gatk --java-options "-Xmx32g -Xms32g" \
     GenotypeGVCFs \
-    -R /home/vetlinux05/Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
+    -R /home//Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
     --variant "gendb://my_database_chr2L" \
     -O 2L_genotyped.vcf.gz \
-    --tmp-dir /home/vetlinux05/Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
+    --tmp-dir /home//Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
     >> 2L_genotype.log 2>&1
 
 # For chromosome 2R
 gatk --java-options "-Xmx32g -Xms32g" \
     GenotypeGVCFs \
-    -R /home/vetlinux05/Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
+    -R /home//Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
     --variant "gendb://my_database_chr2R" \
     -O 2R_genotyped.vcf.gz \
-    --tmp-dir /home/vetlinux05/Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
+    --tmp-dir /home//Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
     >> 2R_genotype.log 2>&1
 
 # For chromosome 3L
 gatk --java-options "-Xmx32g -Xms32g" \
     GenotypeGVCFs \
-    -R /home/vetlinux05/Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
+    -R /home//Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
     --variant "gendb://my_database_chr3L" \
     -O 3L_genotyped.vcf.gz \
-    --tmp-dir /home/vetlinux05/Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
+    --tmp-dir /home//Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
     >> 3L_genotype.log 2>&1
 
 # For chromosome 3R
 gatk --java-options "-Xmx32g -Xms32g" \
     GenotypeGVCFs \
-    -R /home/vetlinux05/Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
+    -R /home//Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
     --variant "gendb://my_database_chr3R" \
     -O 3R_genotyped.vcf.gz \
-    --tmp-dir /home/vetlinux05/Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
+    --tmp-dir /home//Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
     >> 3R_genotype.log 2>&1
 
 # For chromosome 4
 gatk --java-options "-Xmx32g -Xms32g" \
     GenotypeGVCFs \
-    -R /home/vetlinux05/Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
+    -R /home//Siva/raw_data/simulans/dsimM252v1.2+microbiome.fa.gz \
     --variant "gendb://my_database_chr4" \
     -O 4_genotyped.vcf.gz \
-    --tmp-dir /home/vetlinux05/Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
+    --tmp-dir /home//Siva/raw_data/test/signor/cut/all_gvcfs/tmp_vcf \
     >> 4_genotype.log 2>&1
 
